ee5538db9adb65c4de1284d5939bf8482f2e38e2
Ritam Chakraborty <104848386+0LEUM@users.noreply.github.com>
2025-08-07 23:54:45 +0530

    Add files via upload

---
+++ apps/api/src/app.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common'
+import { add } from '@autospace/sample-lib'
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!' + add(1, 2) // Example usage of the imported function
+  }
+}
