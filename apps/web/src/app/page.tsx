d87b4940ec0f314b2e6606f825b89abf95b7efdc
0leum <ritam.d3v@gmail.com>
2025-08-08 11:06:27 +0530

    validation script

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,5 @@
-import { add } from '@autospace/sample-lib'
+import { add } from "@autospace/sample-lib";
 
 export default function Home() {
-  return <main>Hello {add(343, 3)}</main>
+  return <main>Hello {add(343, 3)}</main>;
 }
